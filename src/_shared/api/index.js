123823d8b4d300a4a886b7543a4fd978001c0548
Renaud Racinet <renaud.racinet@estiam.com>
2019-11-07 20:19:10 +0100

    feat(project): Show a result
    add Bootstrap, react-bootstrap and webpack-cli modules
    create Result functional component
    
    Result component
    display Paris, France in a card with
    icon, temperatures, weather, sunrise and sunset

---
+++ src/_shared/api/index.js
@@ -0,0 +1,6 @@
+export const API_URL = 'http://api.openweathermap.org/data/2.5/',
+      			 API_UNIT_SYSTEM = '&units=metric',
+						 DEFAULT_LANG = '&lang=fr',
+						 KEY = '&appid=f1a56e9a6c73ef29fc48d5c1a8ec846f';
+
+export default { API_URL, API_UNIT_SYSTEM, DEFAULT_LANG, KEY };
